c67dc11699fcab361f7ec5306fe0fa0f105f806d
JminJ <jminju254@gmail.com>
2025-06-27 10:15:46 +0000

    env 리소스 로드 코드

--- src/arxiv_paper_mcp/config/global_resources.py
+++ src/arxiv_paper_mcp/config/global_resources.py
@@ -1,5 +1,11 @@
 import os
 
+from dotenv import find_dotenv, load_dotenv
+
+load_dotenv(find_dotenv(), override=True)
+
 USING_MODEL_INFO=os.getenv("USING_MODEL_INFO")
 MODEL_TEMPERATURE=float(os.getenv("MODEL_TEMPERATURE", "0.0"))
 MODEL_SEED=int(os.getenv("MODEL_SEED", "256"))
+
+PDF_DOWNLOAD_PATH=os.getenv("PDF_DOWNLOAD_PATH")
