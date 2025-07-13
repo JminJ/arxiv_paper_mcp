0d6a848b3a2ae940a7fe06105df4f97eb4d18453
JminJ <jminju254@gmail.com>
2025-07-13 11:12:55 +0000

    env 업데이트 반영

--- src/arxiv_paper_mcp/config/global_resources.py
+++ src/arxiv_paper_mcp/config/global_resources.py
@@ -8,4 +8,5 @@ USING_MODEL_INFO=os.getenv("USING_MODEL_INFO")
 MODEL_TEMPERATURE=float(os.getenv("MODEL_TEMPERATURE", "0.0"))
 MODEL_SEED=int(os.getenv("MODEL_SEED", "256"))
 
-PDF_DOWNLOAD_PATH=os.getenv("PDF_DOWNLOAD_PATH")
+PDF_DOWNLOAD_PATH=str(os.getenv("PDF_DOWNLOAD_PATH", "./src/arxiv_paper_mcp/data/paper_pdf"))
+PDF_SECTION_SAVE_PATH=str(os.getenv("PDF_SECTION_SAVE_PATH", "./src/arxiv_paper_mcp/data/paper_sections"))
