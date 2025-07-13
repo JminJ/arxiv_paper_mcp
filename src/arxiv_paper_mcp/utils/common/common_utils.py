4ed5099a9e045a9ea24ea1e2fe6100213974d53c
JminJ <jminju254@gmail.com>
2025-07-13 11:13:43 +0000

    공통 utils 추가

---
+++ src/arxiv_paper_mcp/utils/common/common_utils.py
@@ -0,0 +1,28 @@
+import os
+from pathlib import Path
+
+
+def check_directory(target_path:str)->bool:
+    """디렉토리가 존재하는지 bool 리턴
+    """
+    return os.path.exists(target_path)
+
+def mkdir_directory(target_path:str):
+    """대상 디렉토리 경로 생성
+    """
+    path = Path(target_path)
+    path.mkdir(parents=True, exist_ok=True)
+
+def extract_paper_id_from_path(paper_pdf_file_path:str)->str:
+    """논문 pdf 파일 경로에서 arxiv paper id를 추출
+
+    Args:
+        paper_pdf_file_path (str): 대상 논문 pdf 파일 경로
+
+    Returns:
+        str: 추출된 arxiv paper id
+    """
+    paper_file_name = paper_pdf_file_path.split("/")[-1]
+    arxiv_paper_id = ".".join(paper_file_name.split(".")[:-1]) # 파일 이름 내 '.'가 존재할 수 있기 때문에
+
+    return arxiv_paper_id
