ec00c727abd5ca4ff89c4014c0bfde753d084a16
JminJ <jminju254@gmail.com>
2025-07-15 13:54:33 +0000

    논문 id 기반 목차 디렉토리 경로 반환 함수 추가

--- src/arxiv_paper_mcp/utils/common/common_utils.py
+++ src/arxiv_paper_mcp/utils/common/common_utils.py
@@ -1,6 +1,8 @@
 import os
 from pathlib import Path
 
+from src.arxiv_paper_mcp.config.global_resources import PDF_SECTION_SAVE_PATH
+
 
 def check_directory(target_path:str)->bool:
     """디렉토리가 존재하는지 bool 리턴
@@ -26,3 +28,27 @@ def extract_paper_id_from_path(paper_pdf_file_path:str)->str:
     arxiv_paper_id = ".".join(paper_file_name.split(".")[:-1]) # 파일 이름 내 '.'가 존재할 수 있기 때문에
 
     return arxiv_paper_id
+
+def get_target_paper_section_dir_path(paper_id:str)->str:
+    """논문 id 기반으로 목차 디렉토리 경로를 반환합니다.
+
+    Args:
+        paper_id (str): 대상 논문 paper id
+
+    Returns:
+        str: 목차 파일 경로
+    """
+    return os.path.join(PDF_SECTION_SAVE_PATH, paper_id)
+
+def get_target_paper_section_file_path(paper_id:str)->str:
+    """논문 id 기반으로 목차 파일 경로를 반환합니다.
+
+    Args:
+        paper_id (str): 대상 논문 paper id
+
+    Returns:
+        str: 목차 파일 경로
+    """
+    temp_paper_pdf_directory_path = get_target_paper_section_dir_path(paper_id)
+    return os.path.join(temp_paper_pdf_directory_path, "sections_info.txt")
+
