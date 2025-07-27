2abb9be9b951b0bbd14471609cf53c0f9b5ada42
JminJ <jminju254@gmail.com>
2025-07-27 12:52:05 +0000

    인자값들 고정값으로 변경

--- src/arxiv_paper_mcp/config/global_resources.py
+++ src/arxiv_paper_mcp/config/global_resources.py
@@ -1,12 +1,8 @@
 import os
 
-from dotenv import find_dotenv, load_dotenv
-
-load_dotenv(find_dotenv(), override=True)
-
 USING_MODEL_INFO=os.getenv("USING_MODEL_INFO")
-MODEL_TEMPERATURE=float(os.getenv("MODEL_TEMPERATURE", "0.0"))
-MODEL_SEED=int(os.getenv("MODEL_SEED", "256"))
+MODEL_TEMPERATURE=0.0
+MODEL_SEED=256
 
-PDF_DOWNLOAD_PATH=str(os.getenv("PDF_DOWNLOAD_PATH", "./src/arxiv_paper_mcp/data/paper_pdf"))
-PDF_SECTION_SAVE_PATH=str(os.getenv("PDF_SECTION_SAVE_PATH", "./src/arxiv_paper_mcp/data/paper_sections"))
+PDF_DOWNLOAD_PATH="/workspaces/arxiv_paper_mcp/src/arxiv_paper_mcp/data/paper_pdf"
+PDF_SECTION_SAVE_PATH="/workspaces/arxiv_paper_mcp/src/arxiv_paper_mcp/data/paper_sections"
