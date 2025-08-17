18adc9da7c6702de1e251638c06a3bff9deec249
JminJ <jminju254@gmail.com>
2025-08-17 13:15:48 +0000

    주석 추가

--- src/arxiv_paper_mcp/config/global_resources.py
+++ src/arxiv_paper_mcp/config/global_resources.py
@@ -5,8 +5,8 @@ from dotenv import find_dotenv, load_dotenv
 load_dotenv(find_dotenv())
 
 USING_MODEL_INFO=os.getenv("USING_MODEL_INFO")
-MODEL_TEMPERATURE=0.0
-MODEL_SEED=256
+MODEL_TEMPERATURE=0.0 # 고정
+MODEL_SEED=256 # 고정
 
-PDF_DOWNLOAD_PATH="/workspaces/arxiv_paper_mcp/src/arxiv_paper_mcp/data/paper_pdf"
-PDF_SECTION_SAVE_PATH="/workspaces/arxiv_paper_mcp/src/arxiv_paper_mcp/data/paper_sections"
+PDF_DOWNLOAD_PATH="/workspaces/arxiv_paper_mcp/src/arxiv_paper_mcp/data/paper_pdf" # 고정
+PDF_SECTION_SAVE_PATH="/workspaces/arxiv_paper_mcp/src/arxiv_paper_mcp/data/paper_sections" # 고정
