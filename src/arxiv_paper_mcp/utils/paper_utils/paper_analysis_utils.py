6deab87b180b3f3034f4071817b3c1f4da861483
JminJ <jminju254@gmail.com>
2025-11-16 14:02:29 +0000

    test 코드 제거

--- src/arxiv_paper_mcp/utils/paper_utils/paper_analysis_utils.py
+++ src/arxiv_paper_mcp/utils/paper_utils/paper_analysis_utils.py
@@ -83,38 +83,4 @@ async def analyze_multi_papers(paper_ids:List[str], raw_user_quesiton:str)->str:
 
     return gen_result
 
-
-if __name__ == "__main__":
-    import asyncio
-
-    from icecream import ic
-
-    result = asyncio.run(
-        analyze_target_paper(
-            paper_id="2507.10524",
-            section_names=[
-                "3. Experiments",
-                "3.1. Main Results",
-                "3.2. IsoFLOP Analysis",
-                "3.3. Inference Throughput Evaluation",
-                "4. Ablation Studies",
-                "4.1. Parameter Sharing Strategies",
-                "4.2. Routing Strategies",
-                "4.3. KV Caching Strategies",
-                "5. Analysis",
-                "5.1. Compute-optimal Scaling Analysis",
-                "5.2. Routing Analysis",
-                "5.3. Test-time Scaling Analysis",
-                "6. Related Work",
-                "Adaptive computation.",
-                "Routing mechanism.",
-                "Key-value caching.",
-                "Latent reasoning."
-            ],
-            raw_user_question="각 목차(3. Experiments, 4. Ablation Studies, 5. Analysis, 6. Related Work)와 그 하위 세부 섹션들의 내용을 모두 설명해줘."
-        )
-    )
-
-    ic(result)
-
         
